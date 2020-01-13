6ae200b8b4412a8b56f7031475ffdd499f2c0ea2
sirsuccess <kanuamani@gmail.comgit config --global user.email kanuamani@gmail.comgit config --global user.email kanuamani@gmail.com>
2020-01-14 00:58:46 +0100

    created constant colors

---
+++ constants/colors.js
@@ -0,0 +1,13 @@
+export default {
+  white: "#FFFFFF",
+  lightGray: "#00000017",
+  gray: "#646A65",
+  green1: "#75E977",
+  green2: "#70E56D",
+  green3: "#5EE555",
+  green4: "#2DE427",
+  green5: "#8DEF6F",
+  green: "#40C518",
+  lightGreen: "#A8F192",
+  deepGreen: "#063008"
+};
