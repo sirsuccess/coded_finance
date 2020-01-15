5257ca247f850b1a7d34db39a88658bfe9dc2461
sirsuccess <kanuamani@gmail.comgit config --global user.email kanuamani@gmail.comgit config --global user.email kanuamani@gmail.com>
2020-01-15 22:03:02 +0100

    created component to handle big text

---
+++ components/bigText.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Color from "../constants/colors";
+
+export default function BigText({ text }) {
+  return (
+    <View>
+      <Text style={styles.BigText}>{text}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  BigText: {
+    color: Color.deepGreen,
+    fontSize: 43,
+    fontWeight: "bold",
+    marginTop: 100
+  }
+});
